565815921488298566ff8e20e10213a69a1cc8ba
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-07 17:53:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box.jsx";
+import "./App.css";
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => setMonsters(users))
+    );
+  }, []);
+
+  const filteredMonters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Pokedex</h1>
+      <SearchBox
+        placeholder="Search Monter"
+        handleChange={
+          (e) => setSearchField(e.target.value)
+          // e => this.setState({ searchField: e.target.value }),
+          // () => console.log(this.state)
+        }
+      />
+      <CardList monsters={filteredMonters} />
     </div>
   );
 }
